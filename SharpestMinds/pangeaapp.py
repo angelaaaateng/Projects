a55d772a96783f64249e8583d8fc3d0b125e00fc
Amber Teng <31015696+angelaaaateng@users.noreply.github.com>
2019-04-17 22:49:34 -0500

    flask api version 1

---
+++ SharpestMinds/pangeaapp.py
@@ -0,0 +1,16 @@
+import Pangea_RS_Cleaned
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+#input is new user post and pickle
+class Pangea_RS_Cleaned(Resource):
+    def get(self, user_post, second_number):
+        return {'Recommendations': Pangea_RS_Cleaned.generate_recommendations(user_post)}
+#return depends on what pangea wants
+api.add_resource(Pangea_RS_Cleaned, '/Pangea_RS_Cleaned/<user_post>')
+
+if __name__ == '__main__':
+     app.run()
