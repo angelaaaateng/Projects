b86f50e5d187680ce32b718c0ab6541ab7f475ba
Angela Teng <angelamarieteng@gmail.com>
2019-04-22 13:14:22 -0500

    flask edits

--- SharpestMinds/pangeaapp.py
+++ SharpestMinds/pangeaapp.py
@@ -7,11 +7,11 @@ api = Api(app)
 
 #input is new user post and pickle
 class Pangea_RS_Cleaned(Resource):
-    def get(self, user_post, second_number):
+    def get(self, user_post):
         return {'Recommendations': Pangea_RS_Cleaned.generate_recommendations(user_post)}
 #return depends on what pangea wants
 #api.add_resource(Pangea_RS_Cleaned, '/Pangea_RS_Cleaned/<user_post>')
 api.add_resource(Pangea_RS_Cleaned, '/Pangea_RS_Cleaned/')
 
 if __name__ == '__main__':
-     app.run()
+     app.run(debug=True)
