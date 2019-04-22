f1aa0820862c44fc8d55c5eaf36678efe27238ca
Angela Teng <angelamarieteng@gmail.com>
2019-04-22 18:24:15 -0500

    Update pangeaapp.py

--- SharpestMinds/pangeaapp.py
+++ SharpestMinds/pangeaapp.py
@@ -1,9 +1,26 @@
+import gensim
+from flask import Flask
+import io
+import flask
+import json
+
 import Pangea_RS_Cleaned
 from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+
+model = None
+
+def load_model():
+    # load the pre-trained Keras model (here we are using a model
+    # pre-trained on ImageNet and provided by Keras, but you can
+    # substitute in your own networks just as easily)
+    global model
+    model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary = True)
+
+    print("* Model loaded successfully")
 
 #input is new user post and pickle
 class Pangea_RS_Cleaned(Resource):
