12567c34f69cb200cfa269c5e30e805062a784ea
Angela Teng <angelamarieteng@gmail.com>
2019-05-14 13:32:37 -0500

    flask app updated

--- SharpestMinds/flask_pangea.py
+++ SharpestMinds/flask_pangea.py
@@ -15,62 +15,65 @@ from Pangea_Final_Script import generate_recommendations
 
 '''
 Import Flask class and create an instance of this class
+Note that __name__ is the name of the current class, function,
+method, descriptor or generator instance.
 '''
 app = Flask(__name__)
-#for more information on flask easy startup see http://flask.pocoo.org/docs/1.0/quickstart/
+#for more information on flask easy startup see
+#http://flask.pocoo.org/docs/1.0/quickstart/ ;
+#https://teamtreehouse.com/community/can-someone-help-me-understand-flaskname-a-little-better
 model = None
 
 def load_model():
-    '''
-    load the pre-trained word2vec model and define it as a global variable
-    that we can use after startup
-
-    input: none ; but maximum size of the model
-    '''
+'''
+Load the pre-trained word2vec model and define it as a global variable
+that we can use after startup
+'''
     global model
     model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary = True)
 
     print("* Model loaded successfully")
 
 def find_vocab():
-    '''
-    Returns a model vocuabulary (dict)
-    '''
+'''
+Returns the model vocuabulary.
+
+Output: (dict)
+'''
     return model.vocab
     print("* Model Vocab Loaded")
-    #return(None)
 
-'''
-Specify the app route using a route decorater to
-bind the predict function to a url.
-'''
+
 @app.route("/pangeaapp", methods=["POST"])
 def predict():
-    # initialize the data dictionary that will be returned
-    # view
+'''
+Specify the app route using a route decorater to
+bind the predict function to a url, and then check
+to ensure the json req was properly uploaded to endpoint.
+Use POST method to send HTML from data to server in encrypted manner.
+
+Output: JSON with title and cosine similarity score (dict)
+'''
     data = {"success": False}
     print("* Initialization ok")
-
     # ensure that a json request was properly uploaded to our endpoint
     if flask.request.method == "POST":
+    #flask methods: https://www.tutorialspoint.com/flask/flask_http_methods.htm
         if flask.request.data:
-            # read json title
             title = json.loads(flask.request.data)["title"]
-            #encode('ascii','ignore')
+            #not using encode('ascii','ignore') as it throws an error
             print("*Input Title: ")
             print(title)
             data["recommendations"] = generate_recommendations(title, model)
-            #print(data["recommendations"])
-
             # indicate that the request was a success
             data["success"] = True
-
     # return the data dictionary as a JSON response
     return flask.jsonify(data)
 
 '''
+Checks to see if the name module was called interactively and
+then call the specified function to execute the code.
 '''
-
 if __name__ == "__main__":
     print(("* Loading gensim model and Flask starting server..."
         "please wait until server has fully started"))
