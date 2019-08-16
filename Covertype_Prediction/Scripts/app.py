787872b4b78bce1de96d44621cba2935f06d10ee
Angela Teng <angela_teng@alumni.brown.edu>
2019-08-16 19:54:58 -0400

    working on html display file

--- Covertype_Prediction/Scripts/app.py
+++ Covertype_Prediction/Scripts/app.py
@@ -48,6 +48,8 @@ def form():
 def transform_view():
     # if request.method == "POST":
     #     df = pd.read_csv(request.files.get('data_file'))
+
+    # START COMMENT BLOCK
     print("* Requesting data -- API")
     f = request.files['data_file']
     # print(f)
@@ -66,7 +68,7 @@ def transform_view():
     data, df4, df4_column_names, df_normalized, df_normalized_w_target, X_test_new, y_test_new = preprocess(df)
     print('* Data Preprocessing Complete Flask -- API')
 
-    model = load('./grid_search_optimal.joblib')
+    # model = load('./grid_search_optimal.joblib')
     print('* Joblib model loaded -- API')
     #preprocess(f)
     #def preprocess(csv_file):
@@ -80,16 +82,20 @@ def transform_view():
     # X_train, X_test_new, y_train, y_test_new = initialize_sample(df_normalized_w_target, X_test, y_test)
     # print("* Data Initialized for First Pickle")
 
-    rfc_test_acc, y_pred, class_rept, conf_mat = hyper_param_rf(X_test_new, y_test_new)
-    print("* Hyperparameter search complete -- API")
-    conf_mat = confusion_matrix(y_test_new,y_pred)
-    class_rept = classification_report(y_test_new,y_pred )
+    # rfc_test_acc, y_pred, class_rept, conf_mat = hyper_param_rf(X_test_new, y_test_new)
+    # print("* Hyperparameter search complete -- API")
+    # conf_mat = confusion_matrix(y_test_new,y_pred)
+    # class_rept = classification_report(y_test_new,y_pred )
+    # END COMMENT BLOCK
+
     # print(df4.head())
     # print(df4_column_names)
     # return(data, df4, df4_column_names, df_normalized, df_normalized_w_target, X_test, y_test)
 
     # print('* API: Data Preprocessing Completed')
 
+    class_rept = "hello i am class_rept"
+    conf_mat = "hello i am conf_mat"
     #return confusion matrix
     return("""
         <html>
@@ -97,7 +103,7 @@ def transform_view():
                 <h1> Model Performance Results </h1>
                 <h2><center>Classification Report</h2>
                 <form action="/datafeedback" method="post" enctype="application/x-www-form-urlencoded">
-                <p>{class_rept}</p>
+                <p>{{class_rept}}</p>
                 <h2><center>Confusion Matrix</h2>
                 <p>{conf_mat}</p>
             </body>
