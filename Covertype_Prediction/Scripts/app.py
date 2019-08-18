490fcba111881472d717029668111cb2a1034aba
Angela Teng <angela_teng@alumni.brown.edu>
2019-08-17 23:16:09 -0400

    loading image, working on correct file path - created template folder

--- Covertype_Prediction/Scripts/app.py
+++ Covertype_Prediction/Scripts/app.py
@@ -22,19 +22,21 @@ print(matplotlib.get_backend())
 
 import matplotlib.pyplot as plt
 
-app = Flask(__name__)
+IMAGE_FOLDER = os.path.join('static', 'people_photo')
 
-#model = None
+app = Flask(__name__, template_folder='templates')
+# app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
 
-# def load_model():
-#     '''
-#     Load the pre-trained word2vec model and define it as a global variable
-#     that we can use after startup
-#     '''
-#     global model
-    # model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary = True)
 
-    # print("* Model loaded successfully")
+def load_model():
+    '''
+    Load the model and define it as a global variable
+    that we can use after startup
+    '''
+    global model
+    model = load('./grid_search_optimal.joblib')
+
+    print("* Model loaded successfully")
 
 @app.route('/')
 def form():
@@ -53,42 +55,22 @@ def form():
         </html>
     """)
 
-
-def load_model():
-    '''
-    Load the model and define it as a global variable
-    that we can use after startup
-    '''
-    global model
-    model = load('./grid_search_optimal.joblib')
-    # model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary = True)
-
-    print("* Model loaded successfully")
-
-
 @app.route('/datafeedback', methods=["GET", "POST"])
 def transform_view():
-
-    # START COMMENT BLOCK
     print("* Requesting data -- API")
     f = request.files['data_file']
-    # print(f)
+
     if not f:
         return("No file selected. Please choose a CSV file and try again.")
 
     stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
     csv_input = csv.reader(stream)
     print("* Processing csv_input -- API")
-    # print(stream)
-    # print(csv_input)
     df = pd.DataFrame(csv_input, index=None, columns=None)
-    # df = pd.DataFrame([csv_input], index=None, columns=None)
-    # print(df.head())
 
     data, df4, df4_column_names, df_normalized, df_normalized_w_target, X_test_new, y_test_new = preprocess(df)
     print('* Data Preprocessing Complete Flask -- API')
 
-    # model = load('./grid_search_optimal.joblib')
     print('* Joblib model loaded -- API')
 
 
@@ -103,45 +85,12 @@ def transform_view():
     conf_mat = confusion_matrix(y_test_new,y_pred)
     class_rept = classification_report(y_test_new,y_pred )
     print("* Conf Mat and Class Rept Defined -- API")
-    # END COMMENT BLOCK
 
-    # print(df4.head())
-    # print(df4_column_names)
-    # return(data, df4, df4_column_names, df_normalized, df_normalized_w_target, X_test, y_test)
-
-    # print('* API: Data Preprocessing Completed')
-
-    # class_rept = "hello i am class_rept"
-    # conf_mat = "hello i am conf_mat"
-    #return confusion matrix
-    # return("""
-    #     <html>
-    #         <body>
-    #             <h1> Model Performance Results </h1>
-    #             <h2><center>Classification Report</h2>
-    #             <form action="/datafeedback" method="post" enctype="application/x-www-form-urlencoded">
-    #             <p>{{class_rept}}</p>
-    #             <h2><center>Confusion Matrix</h2>
-    #             <p>{conf_mat}</p>
-    #         </body>
-    #     </html>
-    # """)
     print("* Saving results in an image....")
-    # fig = plt.figure()
-    # plt.matshow(conf_mat)
-    # plt.title('Confusion Matrix')
-    # plt.show()
-    # plt.savefig('./confusion_matrix.jpg')
 
-    return('* CSV File Submitted -- Running API')
-
-    #return(f)
-    # return(rfc_test_acc, y_pred, class_rept, conf_mat)
-    # template_dir = '/Users/angelateng/Documents/GitHub/Projects/Covertype_Prediction/Scripts'
-    # loader = jinja2.FileSystemLoader(template_dir)
-    # environment = jinja2.Environment(loader=loader)
-
-    # return(render_template('page.html', conf_mat=conf_mat, class_rept=class_rept))
+    # return('* CSV File Submitted -- Running API')
+    # full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'confusion_matrix.png')
+    return render_template("page.html", matrix_image = './Scripts/confusion_matrix.png')
 
 
 if __name__ == '__main__':
